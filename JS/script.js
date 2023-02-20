f83c006b624159339f4c0585b6c3bb1bb2ec9386
fabriciozuck <fabricioaraujo4000@gmail.com>
2023-02-20 12:58:04 -0300

    finalizando

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
-console.log("teste");
+const menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+    navMenu.classList.toggle("ativo");
+})
